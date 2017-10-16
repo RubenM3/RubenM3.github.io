0c66bb439a3f0fdb13c4227b7857e51ed138164c
RubenM3 <32261901+RubenM3@users.noreply.github.com>
2017-10-16 14:31:14 +0100

    teste

--- sketch.js
+++ sketch.js
@@ -1,14 +1,26 @@
-
+var frase;
 
 function setup() 
 {
   createCanvas (windowWidth, windowHeight);
+
+frase =  " Olá Mundo!!!!!" ;
+
 }
 
 
 function draw() 
 {
-  background( random(0, 255), random(0, 255), random(0, 255) );
+  background(255);
+  
+
+  textAlign(CENTER, CENTER);
+  textSize(50);
+  textFont("Shrikhand");
+  textLeading(100);
+  text(frase, width/2, height/2, 50); 
+
+
 }
 
 
