367a7adb746f71badfad2db9c5a1ee9f86bc0bdd
RubenM3 <32261901+RubenM3@users.noreply.github.com>
2017-11-10 15:45:46 +0000

    teste

--- sketch.js
+++ sketch.js
@@ -1,30 +1,44 @@
-var frase;
+var yoff = 0.0;  
+var img;
 
-function setup() 
+function preload() 
 {
-  createCanvas (windowWidth, windowHeight);
-
-frase =  " Olá Mundo!!!!!" ;
-
+  img = loadImage("eva.png");
 }
 
+function setup()
+{
+  imageMode(CENTER);
+  image(img, 0, 0);
+  createCanvas(596, 842);
+  frameRate(1);
+  
+}
 
 function draw() 
 {
   background(255);
+  stroke(20);
+  rect (width/2, height/2, 500, 750, 20);
+  rectMode (CENTER);
+  noFill();
+  image(img, 0, 0);
+  image(img, 0, height/2, img.width/2, img.height/2);
+    
+
+  for (var y=0; y<height+10; y+=10) 
+  {
+    beginShape();
+    var xoff = 0;
+    for (var x=0; x<width+10; x+=10) 
+    {
+      var a = noise(xoff, yoff)+1;
+
+      vertex(x, y*a); 
+      xoff += 0.05;
+    }
+    yoff += 0.03;
+    endShape();
+  }
   
-
-  textAlign(CENTER, CENTER);
-  textSize(50);
-  textFont("Shrikhand");
-  textLeading(100);
-  text(frase, width/2, height/2, 50); 
-
-
-}
-
-
-function windowResized() 
-{
-  resizeCanvas (windowWidth, windowHeight);
 }
