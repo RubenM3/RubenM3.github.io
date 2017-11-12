1ce223ffbd5015c47dc27fe746bbe167677bc65b
RubenM3 <32261901+RubenM3@users.noreply.github.com>
2017-11-12 20:01:45 +0000

    gg

--- sketch.js
+++ sketch.js
@@ -19,7 +19,8 @@ function draw()
   rect (width/2, height/2, 500, 750, 20);
   rectMode (CENTER);
   noFill();
-  image( 0, 0 );
+  image(img, width/2, height/2, img.width/2, img.height/2);
+}
   
   for (var y=0; y<height+10; y+=10) 
   {
@@ -35,7 +36,4 @@ function draw()
     yoff += 0.03;
     endShape();
   }
-
- 
-  
 }
